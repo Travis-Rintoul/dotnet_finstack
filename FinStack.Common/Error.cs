88d65687988dfe2241ad9a451453b4b30ed68666
Travis Rintoul <travis.rintoul@gmail.com>
2025-08-25 23:36:48 +1000

    refactored result to use custom Error rather than Excpetions

---
+++ FinStack.Common/Error.cs
@@ -0,0 +1,46 @@
+using System.Text.Json.Serialization;
+
+public enum ErrorSeverity
+{
+    Error,
+    Warning,
+}
+
+public record Error
+{
+    public string Code { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    
+    [JsonIgnore]
+    public ErrorSeverity Severity { get; set; } = ErrorSeverity.Error;
+
+    public Error(string Code)
+    {
+        this.Code = Code;
+    }
+
+    public Error(string Code, string Message)
+    {
+        this.Code = Code;
+        this.Message = Message;
+    }
+
+    public static readonly Error FirstNameRequired =
+        new("FirstNameRequired", "First name is required.");
+
+    public static readonly Error LastNameRequired =
+        new("LastNameRequired", "Last name is required.");
+
+    public static Error UserNotFound(Guid userGuid) =>
+        new("UserNotFound", $"User with id {userGuid} was not found.");
+
+    private static readonly Dictionary<string, Error> _errorsByCode =
+    new()
+    {
+        { FirstNameRequired.Code, FirstNameRequired },
+        { LastNameRequired.Code, LastNameRequired }
+    };
+
+    public static Error? FromCode(string code) =>
+        _errorsByCode.TryGetValue(code, out var error) ? error : null;
+}
--- FinStack.Common/Result.cs
+++ FinStack.Common/Result.cs
@@ -5,30 +5,41 @@ namespace FinStack.Common;
 public class Result<T>
 {
     public T? Value { get; }
-    public IReadOnlyList<Exception> Errors { get; }
+    public IReadOnlyList<Error> Errors { get; }
     public bool IsSuccess => Errors.Count == 0;
     public Result(T value)
     {
         Value = value;
-        Errors = new List<Exception>();
-    }
-    public Result(string error) => Errors = [new Exception(error)];
-    public Result(Exception error) => Errors = [error];
-    public Result(IEnumerable<Exception> errors)
-    {
-        Errors = errors as IReadOnlyList<Exception> ?? errors?.ToList();
-    }
-    public Result(IEnumerable<IdentityError> errors)
-    {
-        Errors = errors.Select(e => new Exception(e.Code)).ToList();
+        Errors = new List<Error>();
     }
 
-    public bool Failed(out IEnumerable<Exception> errors)
+
+    public Result(Error error)
+    {
+        Errors = new List<Error> { error };
+    }
+
+        public Result(IEnumerable<Error> errors)
+    {
+        Errors = errors.ToList();
+    }
+
+    public Result(string code, string message)
+    {
+        Errors = [new Error(code, message)];
+    }
+
+    public Result(IEnumerable<IdentityError> errors)
+    {
+        Errors = errors.Select(e => new Error(e.Code, e.Description)).ToList();
+    }
+
+    public bool Failed(out IEnumerable<Error> errors)
     {
         errors = Errors;
         return !IsSuccess;
     }
-    
+
     public bool Succeeded(out T value)
     {
         if (IsSuccess)
@@ -42,8 +53,8 @@ public class Result<T>
             return false;
         }
     }
-    
-    public TResult Match<TResult>(Func<T, TResult> onSuccess, Func<IEnumerable<Exception>, TResult> onFailure)
+
+    public TResult Match<TResult>(Func<T, TResult> onSuccess, Func<IEnumerable<Error>, TResult> onFailure)
     {
         if (IsSuccess)
         {
@@ -59,8 +70,7 @@ public class Result<T>
 public static class Result
 {
     public static Result<T> Success<T>(T value) => new (value);
-    public static Result<T> Failure<T>(string error) => new(error);
-    public static Result<T> Failure<T>(Exception error) => new(error);
-    public static Result<T> Failure<T>(IEnumerable<Exception> errors) => new(errors);
+    public static Result<T> Failure<T>(Error error) => new(error);
+    public static Result<T> Failure<T>(IEnumerable<Error> errors) => new(errors);
     public static Result<T> Failure<T>(IEnumerable<IdentityError> errors) => new(errors);
 }
