a3ac088aaac8ba2537a6e28f7c87375bf3b3c20a
Travis Rintoul <travis.rintoul@gmail.com>
2025-08-26 03:55:57 +1000

    updated AppDbContext

--- FinStack.Infrastructure/Data/AppDbContext.cs
+++ FinStack.Infrastructure/Data/AppDbContext.cs
@@ -1,15 +1,41 @@
 using Microsoft.EntityFrameworkCore;
 using FinStack.Domain.Entities;
-using FinStack.Infrastructure.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace FinStack.Infrastructure.Data;
 
-public class AppDbContext : IdentityDbContext<AuthUser>
+public class AuthRole : IdentityRole<Guid>
+{
+}
+
+public class AppDbContext : IdentityDbContext<AuthUser, AuthRole, Guid>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<User> Users { get; set; }
+    public DbSet<User> AppUsers { get; set; }
     public DbSet<Job> Jobs { get; set; }
     public DbSet<UserPreference> UserPreferences { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<User>()
+            .ToTable("AppUsers")
+            .HasKey(u => u.UserGuid);
+
+        builder.Entity<User>()
+            .HasOne(u => u.AuthUser)
+            .WithOne(a => a.AppUser)
+            .HasForeignKey<User>(u => u.UserGuid);
+
+        builder.Entity<AuthUser>().ToTable("AuthUsers");
+        builder.Entity<IdentityRole>().ToTable("AuthRoles");
+        builder.Entity<IdentityUserRole<Guid>>().ToTable("AuthUserRoles");
+        builder.Entity<IdentityUserClaim<Guid>>().ToTable("AuthUserClaims");
+        builder.Entity<IdentityUserLogin<Guid>>().ToTable("AuthUserLogins");
+        builder.Entity<IdentityRoleClaim<Guid>>().ToTable("AuthRoleClaims");
+        builder.Entity<IdentityUserToken<Guid>>().ToTable("AuthUserTokens");
+    }
 }
