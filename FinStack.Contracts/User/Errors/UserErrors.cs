46bf417f16eb8a9a73fd4de2101795a1f43e32c3
Travis Rintoul <travis.rintoul@gmail.com>
2025-08-30 03:10:32 +1000

    updated error message

--- FinStack.Contracts/User/Errors/UserErrors.cs
+++ FinStack.Contracts/User/Errors/UserErrors.cs
@@ -1,7 +1,7 @@
 public static class UserErrors
 {
     public static readonly Error NotFound =
-        new("User.NotFound", "User with id {id} was not found.");
+        new("User.NotFound", "Unable to find user");
 
     public static readonly Error AlreadyExists =
         new("User.AlreadyExists", "User with email {email} already exists.");
