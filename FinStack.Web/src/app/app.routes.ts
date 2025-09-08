c8e0b7b8b84758e40177e225e5c3effe52e1b8e0
Travis Rintoul <travis.rintoul@gmail.com>
2025-09-08 20:49:22 +1000

    added app and css files

--- FinStack.Web/src/app/app.routes.ts
+++ FinStack.Web/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MAIN_ROUTES } from '../areas/main.routes';
+import { AUTH_ROUTES } from '../areas/auth.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    ...MAIN_ROUTES,
+    ...AUTH_ROUTES,
+];
--- FinStack.Web/src/app/app.ts
+++ FinStack.Web/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ApiPrefixInterceptor } from '../core/interceptors/api-prefix.interceptor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
