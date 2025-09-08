7dcbbe7b55abf2b59838851de6e9253befecf5e6
Travis Rintoul <travis.rintoul@gmail.com>
2025-09-08 20:45:14 +1000

    added insights feature component

---
+++ FinStack.Web/src/features/insights/insights.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-insights',
+  imports: [],
+  templateUrl: './insights.component.html',
+  styleUrl: './insights.component.scss',
+  standalone: true
+})
+export class InsightsComponent {
+
+}
