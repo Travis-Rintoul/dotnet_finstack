1fccd09bc9175ea221a0d2ed57513cfcb61b5ed1
Travis Rintoul <travis.rintoul@gmail.com>
2025-09-08 20:45:00 +1000

    added invest feature component

---
+++ FinStack.Web/src/features/invest/invest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invest',
+  imports: [],
+  templateUrl: './invest.component.html',
+  styleUrl: './invest.component.scss',
+  standalone: true
+})
+export class InvestComponent {
+
+}
