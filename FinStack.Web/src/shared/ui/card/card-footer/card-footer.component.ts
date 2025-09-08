dfe52b209a223bc140d35675d21c87045bf7dc4d
Travis Rintoul <travis.rintoul@gmail.com>
2025-09-08 20:39:34 +1000

    added card component

---
+++ FinStack.Web/src/shared/ui/card/card-footer/card-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-card-footer',
+  imports: [],
+  templateUrl: './card-footer.component.html',
+  styleUrl: './card-footer.component.scss'
+})
+export class CardFooterComponent {
+
+}
