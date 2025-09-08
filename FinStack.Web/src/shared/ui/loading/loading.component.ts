7703ab9ccb6beb719379cb6240d88630196af7b5
Travis Rintoul <travis.rintoul@gmail.com>
2025-09-08 20:39:47 +1000

    added loading component

---
+++ FinStack.Web/src/shared/ui/loading/loading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-loading',
+  imports: [],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss',
+  standalone: true
+})
+export class LoadingComponent {
+  
+}
